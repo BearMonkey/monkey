e8106488de6b30b59babea43abf5c1ef6065bb0e
monkey_win <monkey_win@sina.com>
2018-03-29 19:08:29 +0800

    first commit monkey

---
+++ monkey-www/src/main/java/org/monkey/interceptor/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package org.monkey.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * 登录验证拦截器
+ * @author chenc
+ *
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter{
+    
+    private Logger log = Logger.getLogger(LoginInterceptor.class); 
+    
+    @Override  
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {  
+        String url = request.getRequestURL().toString();
+        log.info("url:" + url);
+        return true;
+    }
+}
