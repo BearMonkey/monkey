79e8f4289fc84d91df9f6712a43051b7fbc466dc
monkey <monkey_win@sina.com>
2024-03-23 07:42:55 +0800

    给账号管理模块添加网关

--- mq-tx-account/src/main/java/org/monkey/mqtx/account/controller/AccountController.java
+++ mq-tx-account/src/main/java/org/monkey/mqtx/account/controller/AccountController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/mqtx")
+@RequestMapping("/mqtx/account")
 @Slf4j
 public class AccountController {
 
